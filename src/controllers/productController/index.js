438e7772ac16e68b4c083ffa3a1817d26c18f189
Tyler-Oreskey <toreskey@gmail.com>
2023-03-14 17:29:35 +0000

    removed unneeded code checking for python and postman

--- src/controllers/productController/index.js
+++ src/controllers/productController/index.js
@@ -20,22 +20,11 @@ module.exports = {
 
     create: async (req, res, next) => {
         try {
-            let request;
+            const { name, upcCode } = req.body;
 
-            // to support both postman and python requests
-            if (Object.keys(req.query).length > 0) {
-                // express recognizes python get req. params as a "query" object in the req.
-                request = req.query;
-            }
-            else {
-                request = req.body;
-            }
-
-            if (!request.name || !request.upcCode) {
+            if (!name || !upcCode) {
                 return res.status(404).json("Missing required name and/or upc code.");
             }
-
-            const { name, upcCode } = request;
 
             // check if product already exists.
             const duplicate = await productModel.getByUPCCode(upcCode);
